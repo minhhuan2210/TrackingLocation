305f0346e2cd3fb54b34597d7700aed9b7b50ccc
Huan Lu <minhhuan2210@gmail.com>
2024-03-06 11:08:30 +0700

    feat: integrate tracking location and local push notification

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,36 +1,122 @@
 import {
   FlatList,
   Image,
-  ListRenderItem,
+  Linking,
+  PermissionsAndroid,
   Pressable,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {IconMap} from '../assets';
+import Geolocation from '@react-native-community/geolocation';
+import PushNotification from 'react-native-push-notification';
 
 type LocationInfo = {
-  id: number;
-  name: string;
+  latitude: number;
+  longitude: number;
+};
+
+type Position = {
+  coords: {
+    latitude: number;
+    longitude: number;
+    altitude: number | null;
+    accuracy: number;
+    altitudeAccuracy: number | null;
+    heading: number | null;
+    speed: number | null;
+  };
+  timestamp: number;
+};
+
+type Error = {
+  code: number;
+  message: string;
+  PERMISSION_DENIED: number;
+  POSITION_UNAVAILABLE: number;
+  TIMEOUT: number;
+};
+
+type PositionOptions = {
+  timeout?: number;
+  maximumAge?: number;
+  enableHighAccuracy?: boolean;
 };
 
 const HomeScreen = () => {
-  const mockData: LocationInfo[] = [
-    {id: 1, name: 'A'},
-    {id: 2, name: 'B'},
-    {id: 3, name: 'C'},
-    {id: 4, name: 'D'},
-  ];
-
   const [data, setData] = useState<LocationInfo[]>([]);
+  const [currentLocation, setCurrentLocation] = useState<LocationInfo>({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  const requestLocationPermission = () => {
+    // try {
+    //   const isGranted = await PermissionsAndroid.request(
+    //     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    //     {
+    //       title: 'Location Permission',
+    //       message: 'this app needs access to your location',
+    //       buttonPositive: 'Ok',
+    //       buttonNegative: 'Cancel',
+    //       buttonNeutral: 'Ask me later',
+    //     },
+    //   );
+
+    //   if (isGranted === PermissionsAndroid.RESULTS.GRANTED) {
+    //     console.log('success');
+    //   } else {
+    //     console.log('permission denied');
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    Geolocation.requestAuthorization(
+      (success: any) => {
+        console.log('success', success);
+      },
+      (error: Error) => {
+        console.log('permission denied', JSON.stringify(error, null, 2));
+      },
+    );
+  };
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position: Position) => {
+        const {latitude, longitude} = position.coords;
+        setData(prev => [...prev, {latitude, longitude}]);
+        setCurrentLocation({latitude, longitude});
+      },
+      (error: Error) => {
+        console.log('error happened', JSON.stringify(error, null, 2));
+      },
+      {
+        timeout: 15000,
+        maximumAge: 10000,
+        enableHighAccuracy: true,
+      },
+    );
+  };
+
+  const openGoogleMap = () => {
+    const url = `https://www.google.com/maps/search/?api=1&query=${currentLocation.latitude},${currentLocation.longitude}`;
+    Linking.openURL(url);
+  };
 
   const renderItem = ({item}: {item: LocationInfo}) => {
     return (
-      <View style={styles.itemContainer} key={item.id}>
+      <View style={styles.itemContainer} key={'item.id'}>
         <View style={styles.info}>
-          <Text style={styles.itemText}>{item.name}</Text>
-          <Image style={styles.image} source={IconMap}></Image>
+          <View>
+            <Text style={styles.itemText}>latitude: {item.latitude}</Text>
+            <Text style={styles.itemText}>longitude: {item.longitude}</Text>
+          </View>
+          <Pressable hitSlop={40} onPress={openGoogleMap}>
+            <Image style={styles.image} source={IconMap}></Image>
+          </Pressable>
         </View>
         <View style={styles.actions}>
           <Pressable style={styles.actionItem}>
@@ -47,14 +133,38 @@ const HomeScreen = () => {
     );
   };
 
+  const sendNotification = () => {
+    PushNotification.localNotification({
+      title: 'Stop Moving Detected',
+      message:
+        "Hey, it's seems that you're stop moving. \n" +
+        'Please tap on this notification to stop sharing your location with the app.',
+      autoCancel: false,
+    });
+  };
+
   const activeTrackingLocation = () => {
-    setData(mockData);
+    requestLocationPermission();
+    let timeout = 0;
+    const maximumAge = 10 * 1000; // 10 mins
+
+    const id = setInterval(() => {
+      getCurrentLocation();
+      timeout += 8000;
+      console.log('timeout', timeout);
+      if (timeout >= maximumAge) {
+        sendNotification();
+        clearInterval(id);
+      }
+    }, 8000);
   };
 
   const renderEmptyComponent = () => {
     return (
       <View style={styles.emptyCompContainer}>
-        <Pressable style={styles.emptyActionButton} onPress={activeTrackingLocation}>
+        <Pressable
+          style={styles.emptyActionButton}
+          onPress={activeTrackingLocation}>
           <Text style={styles.emptyTextAction}>Start tracking location</Text>
         </Pressable>
       </View>
@@ -88,7 +198,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   itemText: {
-    fontSize: 30,
+    fontSize: 20,
   },
   info: {
     flexDirection: 'row',
@@ -97,6 +207,7 @@ const styles = StyleSheet.create({
   image: {
     height: 40,
     width: 40,
+    marginRight: 20,
   },
   actions: {
     flexDirection: 'row',
